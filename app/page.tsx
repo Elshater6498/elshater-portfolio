f448b5fec28ee0ee5999e6bb3153acc2cd2925b4
Mohamed Elshater <107942995+Elshater6498@users.noreply.github.com>
2024-10-26 17:23:17 +0300

    frist commit

--- app/page.tsx
+++ app/page.tsx
@@ -49,12 +49,11 @@ export default function Home() {
               Available for Work
             </Badge>
             <div className="bg-[#F6F8FA] dark:bg-[#115e3b] h-6 w-6 lg:w-fit lg:px-3 rounded-full flex items-center justify-center gap-x-2">
-            <div className="w-2 h-2 rounded-full animate-ping dark:animate-none bg-[#28C780]" />
-            <p className="text-[#2b9364] hidden lg:flex text-xs font-semibold uppercase">
-              Available for work
-            </p>
-          </div>
-        </div>
+              <div className="w-2 h-2 rounded-full animate-ping dark:animate-none bg-[#28C780]" />
+              <p className="text-[#2b9364] hidden lg:flex text-xs font-semibold uppercase">
+                Available for work
+              </p>
+            </div>
           </motion.div>
           <motion.div variants={item} className="space-y-2">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
